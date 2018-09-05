e4de265c908cf1c2ac807404c6b7fc87385b8c44
hebeon <hebeon@hebeon-OptiPlex-960>
2018-09-05 18:32:18 +0530

    stage1-slide 2-all execute code buttons and verify and displaying output code updated

--- java/src/Baseclass/Clang.java
+++ java/src/Baseclass/Clang.java
@@ -14,7 +14,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Clang  {
 	public static WebDriver driver;
-	//public static WebDriver driver;
 		public static void login() throws IOException, InterruptedException{
 			FileInputStream fis = new FileInputStream("/home/hebeon/git/repository/java/config/config.properties");
 			Properties prop = new Properties();
@@ -38,12 +37,29 @@ public static void stage1() throws InterruptedException {
 	driver.findElement(By.xpath("//body[@id='kids-section']/form[@id='form1']/section[@id='main_content']/div[@class='container-fluid margin-l-r']/div[@class='row']/div[@id='right-layout']/div[@id='page-content']/div[@id='tdNestedPageContent']/div[@class='panel panel-primary']/div[@id='description']/div[2]/span[1]")).click();
 }
 public static void nextbutton() throws InterruptedException {
-     driver.findElement(By.xpath("//a[@id='btnPrev']")).click();
+     driver.findElement(By.xpath("//div[@class='col-lg-6 next-exam-btn']//a[@id='btnPrev']")).click();
      Thread.sleep(1000);
+}
+public static void executebutton(String xpath) throws InterruptedException {
+	List<WebElement>buttons=driver.findElements(By.xpath(xpath));
+	int size=buttons.size();
+	System.out.println("size " + size);
+	for(int i=0;i<size;i++) {
+		buttons.get(i).click();
+		Thread.sleep(2000);
+	}
+}
+public static void output(String xpath) {
+List<WebElement>outputs=driver.findElements(By.xpath(xpath));
+int size2=outputs.size();
+for(int i=0;i<size2;i++) {
+	String s=outputs.get(i).getText();
+	System.out.println(s);
+}
 }
 public static void scrolldown() {
      JavascriptExecutor jse = (JavascriptExecutor)driver;
-     jse.executeScript("window.scrollBy(0,300);");
+     jse.executeScript("window.scrollBy(0,600);");
 }
 public static void cc() {
 	 WebElement iframelement =driver.findElement(By.xpath("//iframe[@class='mySlides2 reload-class']"));
@@ -75,21 +91,35 @@ driver.switchTo().defaultContent();
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","/home/hebeon/Downloads/chromedriver");
-	     driver = new ChromeDriver();
-       login();
-       stage1();
-       nextbutton();
-       nextbutton();
-       nextbutton();
-       nextbutton();
-       nextbutton();
-       nextbutton();
-       scrolldown();
-       cc();
-       taketest1();
-       scrolldown();
-       runcode();
+	  System.setProperty("webdriver.chrome.driver","/home/hebeon/Downloads/chromedriver");
+	  driver = new ChromeDriver();
+      login();
+      stage1();
+      nextbutton();
+      scrolldown();
+      WebElement iframe3=driver.findElement(By.xpath("//embed[@class='mySlides2']"));
+  	  driver.switchTo().frame(iframe3);
+      executebutton("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id=8]//div[@class='assignment-div']//div[@class='pull-right']//button[@class='button_medium1']");  
+      executebutton("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='Div2']//div[@class='assignment-div']//div[@class='pull-right']//button[@class='button_medium1']");
+      output("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id=8]//div[@class='assignment-div']//div[@class='col-md-12']//div[@class='outputresultbox']");
+      output("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='Div2']//div[@class='assignment-div']//div[@class='col-md-12']//div[@class='outputresultbox']");
+//      Thread.sleep(2000);
+//      scrolldown();
+//      nextbutton();
+//      executebutton("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='5']//div[@class='assignment-div']//div[@class='pull-right']//button[@class='button_medium1']");
+//      executebutton("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='9']//div[@class='assignment-div']//div[@class='pull-right']//button[@class='button_medium1']");
+//      output("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='5']//div[@class='assignment-div']//div[@class='col-md-12']//div[@class='outputresultbox']");
+//      output("//body[@class='noselect']//div[@id='body-content']//div[@class='panel-body content-page-control']//div[@id='9']//div[@class='assignment-div']//div[@class='col-md-12']//div[@class='outputresultbox']");
+//      
+      //       nextbutton();
+//       nextbutton();
+//       nextbutton();
+//       nextbutton();
+//       scrolldown();
+//       cc();
+//       taketest1();
+//       scrolldown();
+//       runcode();
        
        
 	}
